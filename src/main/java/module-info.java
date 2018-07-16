90d668f201296213c1a3399459344a86b8b2e532
dermatologist <github@gulfdoctor.net>
2018-07-15 20:19:19 -0400

    module-info.java added.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module nuprescribe {
+    requires java.persistence;
+    requires javafx.graphics;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires javafx.fxml;
+    requires slf4j.api;
+    requires javafx.controls;
+    requires spring.data.jpa;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires java.annotation;
+}
